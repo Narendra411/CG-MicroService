dd97c9047558c74f485a9688995fedca029e1379
Narendra411 <54699766+Narendra411@users.noreply.github.com>
2021-05-14 12:01:52 +0530

    MS 06

---
+++ 06.MS-CreatingTask/src/main/java/com/cap/Application.java
@@ -0,0 +1,36 @@
+package com.cap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableTask
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		
+		
+	}
+		@Bean
+		public TollProcessTask tollProcessTask() {
+			return new TollProcessTask();
+	
+	}
+		
+		public class TollProcessTask implements CommandLineRunner{
+
+			@Override
+			public void run(String... args) throws Exception {
+				
+				for(String i : args)
+				System.out.println("Arguments entered in Command Line Runner\t"+i);
+			}
+			
+			
+		}
+
+}
